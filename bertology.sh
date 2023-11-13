e4c84aadae6d06ce2581bb6d10848811cf744860
lorens69 <21103853@usc.edu.ph>
2023-11-13 14:51:02 +0800

    Create bertology.sh

---
+++ bertology.sh
@@ -0,0 +1,11 @@
+composer install
+npm install
+composer require realrashid/sweet-alert
+php artisan vendor:publish --provider="RealRashid\SweetAlert\SweetAlertServiceProvider"
+cp .env.example .env
+php artisan key:generate
+composer require open-admin-org/open-admin
+php artisan vendor:publish --provider="OpenAdmin\Admin\AdminServiceProvider"
+php artisan admin:install
+composer require open-admin-ext/helpers
+php artisan admin:import helpers
