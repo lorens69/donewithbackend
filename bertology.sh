c3212be5043a1126479d88719b2172d61586ef42
lorens69 <21103853@usc.edu.ph>
2023-11-13 15:57:57 +0800

    Update bertology.sh

--- bertology.sh
+++ bertology.sh
@@ -11,3 +11,7 @@ php artisan vendor:publish --provider="OpenAdmin\Admin\AdminServiceProvider"
 php artisan admin:install
 composer require open-admin-ext/helpers
 php artisan admin:import helpers
+
+echo "DONE DOWNLOADING PRE REQUISITES! OPEN VS CODE NOW! - lorens"
+
+exit 1
