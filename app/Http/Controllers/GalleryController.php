f255a31e6843b05cfe7293be077bbb0c050050d5
shobi_umaran <21103795@usc.edu.ph>
2023-11-27 18:28:49 +0800

    update databaseschema(added galleries table) gallery page to dynamically show gallery data

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -5,42 +5,13 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Collection;
 use Illuminate\Http\Request;
+use App\Models\Gallery;
 
 class GalleryController extends Controller
 {
     public function showGallery()
     {
-        $gallerys = [
-    ["galleryName" => "Car Alarm Installation", "galleryPrice" => 15000],
-    ["galleryName" => "Car Sound Set Up", "galleryPrice" => 12000],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 8000],
-    ["galleryName" => "Car Tinting", "galleryPrice" => 10000],
-    ["galleryName" => "Car Check Ups", "galleryPrice" => 5000],
-    ["galleryName" => "Car Alarm Malfunction Rescue", "galleryPrice" => 7000],
-    ["galleryName" => "Car Head Light Installations", "galleryPrice" => 9000],
-    ["galleryName" => "Car Stereo Installation", "galleryPrice" => 11000],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 8000],
-    ["galleryName" => "Car Alarm Installation", "galleryPrice" => 1232134],
-    ["galleryName" => "Car Sound Set Up", "galleryPrice" => 123],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 8000],
-    ["galleryName" => "Car Tinting", "galleryPrice" => 3],
-    ["galleryName" => "Car Check Ups", "galleryPrice" => 1],
-    ["galleryName" => "Car Alarm Malfunction Rescue", "galleryPrice" => 12],
-    ["galleryName" => "Car Head Light Installations", "galleryPrice" => 9000],
-    ["galleryName" => "Car Stereo Installation", "galleryPrice" => 11000],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 123213],
-    ["galleryName" => "Car Alarm Installation", "galleryPrice" => 15000],
-    ["galleryName" => "Car Sound Set Up", "galleryPrice" => 12000],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 12345123],
-    ["galleryName" => "Car Tinting", "galleryPrice" => 10000],
-    ["galleryName" => "Car Check Ups", "galleryPrice" => 5000],
-    ["galleryName" => "Car Alarm Malfunction Rescue", "galleryPrice" => 7000],
-    ["galleryName" => "Car Head Light Installations", "galleryPrice" => 9000],
-    ["galleryName" => "Car Stereo Installation", "galleryPrice" => 11000],
-    ["galleryName" => "Troubleshoot Sound System", "galleryPrice" => 8000]
-        ];
-
-      // Set the number of items per page (e.g., 9 items per page)
+    $gallerys = Gallery::all();
     $perPage = 9;
 
     // Create a new collection from the gallerys array
---
+++ galleries.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2023 at 11:22 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bertology`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galleries`
+--
+
+CREATE TABLE `galleries` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `album_type` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `galleries`
+--
+
+INSERT INTO `galleries` (`id`, `title`, `description`, `image`, `album_type`, `created_at`, `updated_at`) VALUES
+(1, 'title', 'description', 'images/DisasterGirl_2.jfif', 'images', '2023-11-27 00:28:52', '2023-11-27 00:28:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `galleries`
+--
+ALTER TABLE `galleries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `galleries`
+--
+ALTER TABLE `galleries`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
