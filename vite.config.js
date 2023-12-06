2470f4d2af8c1ff213ba535c12becc3a75a97b4b
lorens69 <21103853@usc.edu.ph>
2023-12-06 09:35:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel([
             'resources/js/app.js',
-            'resources/js/style.css',
+            'public/CSS/style.css',
         ]),
     ],
     resolve: {
