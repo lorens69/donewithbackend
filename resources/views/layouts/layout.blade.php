652219788948cb79fdf536fd113b347bb140f3bf
lorens69 <21103853@usc.edu.ph>
2023-12-07 16:26:52 +0800

    deploy

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title', 'Default Title')</title>
     <script src="https://kit.fontawesome.com/2ecaabd2c2.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="CSS/style.css">
     {{-- @vite(['public/js/app.js']) --}}
 </head>
 
