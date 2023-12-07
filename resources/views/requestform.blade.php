3d81d39f9cb74bea8d2074ff7d154aa283984dd9
lorens69 <21103853@usc.edu.ph>
2023-12-07 13:55:42 +0800

    reqiuestform

--- resources/views/requestform.blade.php
+++ resources/views/requestform.blade.php
@@ -114,7 +114,7 @@
                 <div class="requestform">
                     <h5 class="text-center mb-4" style="font-size: 30px;color: rgb(54, 49, 49)"> BERTOLOGY CAR SERVICE REQUEST FORM</h5>
                     <form action="{{ route('request.post') }}" method="POST">
-                       
+
                         @csrf
 
                         <div class="row justify-content-between text-left">
@@ -184,15 +184,6 @@
                             </div>
                         </div>
                     </form>
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
                 </div>
             </div>
             @endif
