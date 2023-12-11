a20d4daff637888c2597ee848437364493626fa0
Junior112 <david.pael@mlhuillier.com>
2023-12-11 17:12:05 +0800

    12-11-2023 Lebosada contact page and linking footer

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -15,7 +15,7 @@
   <header style="background-color: #1E1B1B; position: sticky; top: 0; z-index: 5; font-family: 'Raleway', sans-serif;">
     <nav class="navbar navbar-expand-sm navbar-dark m-0">
       <div class="container-fluid d-flex justify-content-between">
-        <a class="navbar-brand" href="#"><img src="./img/bertologylogo.png" alt="" width="70px" height="50px"></a>
+        <a class="navbar-brand" href="{{ route('home') }}"><img src="./img/bertologylogo.png" alt="" width="70px" height="50px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav"
           aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
