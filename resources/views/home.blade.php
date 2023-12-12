d01ae9a48f2f261a77de0a445f68ba77f529559d
lorens69 <21103853@usc.edu.ph>
2023-12-12 11:08:06 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -191,7 +191,7 @@
                     <div class="infos">
                         <div class="item-info">
                             <h3 class="title">{{$items->name}}</h3>
-                            <p class="price">{{$items->price}}</p>
+                            <p class="price">Price: Php {{ number_format($product['price']) }}</p>
                         </div>
                         <div class="icons text-center">
                             <form action="{{ route('homeaddToCart', ['productId' => $items->id]) }}" method="POST">
