52cd565df714fb1792ce40f1607b81474d7ad681
lorens69 <21103853@usc.edu.ph>
2023-12-12 08:40:55 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,7 @@
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
 </head>
+
 <body>
 <div class="container-fluid p-0 homecontainer1">
     @include('partials._header')
@@ -256,6 +257,71 @@
 
 @include('sweetalert::alert')
 </body>
+<script>
+
+
+    const prev = document.querySelector("#prev");
+    const next = document.querySelector("#next");
+
+    let carouselVp = document.querySelector("#carousel-vp");
+
+    let cCarouselInner = document.querySelector("#cCarousel-inner");
+    let carouselInnerWidth = cCarouselInner.getBoundingClientRect().width;
+
+    let leftValue = 0;
+
+    // Variable used to set the carousel movement value (card's width + gap)
+    const totalMovementSize =
+      parseFloat(
+        document.querySelector(".cCarousel-item").getBoundingClientRect().width,
+        10
+      ) +
+      parseFloat(
+        window.getComputedStyle(cCarouselInner).getPropertyValue("gap"),
+        10
+      );
+
+    prev.addEventListener("click", () => {
+      if (!leftValue == 0) {
+        leftValue -= -totalMovementSize;
+        cCarouselInner.style.left = leftValue + "px";
+      }
+    });
+
+    next.addEventListener("click", () => {
+      const carouselVpWidth = carouselVp.getBoundingClientRect().width;
+      if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth) {
+        leftValue -= totalMovementSize;
+        cCarouselInner.style.left = leftValue + "px";
+      }
+    });
+
+    const mediaQuery510 = window.matchMedia("(max-width: 510px)");
+    const mediaQuery770 = window.matchMedia("(max-width: 770px)");
+
+    mediaQuery510.addEventListener("change", mediaManagement);
+    mediaQuery770.addEventListener("change", mediaManagement);
+
+    let oldViewportWidth = window.innerWidth;
+
+    function mediaManagement() {
+      const newViewportWidth = window.innerWidth;
+
+      if (leftValue <= -totalMovementSize && oldViewportWidth < newViewportWidth) {
+        leftValue += totalMovementSize;
+        cCarouselInner.style.left = leftValue + "px";
+        oldViewportWidth = newViewportWidth;
+      } else if (
+        leftValue <= -totalMovementSize &&
+        oldViewportWidth > newViewportWidth
+      ) {
+        leftValue -= totalMovementSize;
+        cCarouselInner.style.left = leftValue + "px";
+        oldViewportWidth = newViewportWidth;
+      }
+    }
+
+    </script>
 
 
 <style>
@@ -274,71 +340,7 @@
     }
 </style>
 
-<script>
 
-
-const prev = document.querySelector("#prev");
-const next = document.querySelector("#next");
-
-let carouselVp = document.querySelector("#carousel-vp");
-
-let cCarouselInner = document.querySelector("#cCarousel-inner");
-let carouselInnerWidth = cCarouselInner.getBoundingClientRect().width;
-
-let leftValue = 0;
-
-// Variable used to set the carousel movement value (card's width + gap)
-const totalMovementSize =
-  parseFloat(
-    document.querySelector(".cCarousel-item").getBoundingClientRect().width,
-    10
-  ) +
-  parseFloat(
-    window.getComputedStyle(cCarouselInner).getPropertyValue("gap"),
-    10
-  );
-
-prev.addEventListener("click", () => {
-  if (!leftValue == 0) {
-    leftValue -= -totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
-});
-
-next.addEventListener("click", () => {
-  const carouselVpWidth = carouselVp.getBoundingClientRect().width;
-  if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
-});
-
-const mediaQuery510 = window.matchMedia("(max-width: 510px)");
-const mediaQuery770 = window.matchMedia("(max-width: 770px)");
-
-mediaQuery510.addEventListener("change", mediaManagement);
-mediaQuery770.addEventListener("change", mediaManagement);
-
-let oldViewportWidth = window.innerWidth;
-
-function mediaManagement() {
-  const newViewportWidth = window.innerWidth;
-
-  if (leftValue <= -totalMovementSize && oldViewportWidth < newViewportWidth) {
-    leftValue += totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  } else if (
-    leftValue <= -totalMovementSize &&
-    oldViewportWidth > newViewportWidth
-  ) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  }
-}
-
-</script>
 
 </html>
 @include('partials._footer')
