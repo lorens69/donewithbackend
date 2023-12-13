7b0ec336072bc07d1dd3495618ac55138b0c2f41
lorens69 <21103853@usc.edu.ph>
2023-12-13 15:12:56 +0800

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,6 +13,8 @@
     <title>Contact Us</title>
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+
 </head>
 
 <body>
