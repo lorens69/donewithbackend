0f7c38388c18fe681e086e1bfc7159369be2aa0d
lorens69 <21103853@usc.edu.ph>
2023-12-16 14:54:51 +0800

    title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -220,7 +220,7 @@
                             <div class="col-md-4 style-4">
                                 <div class="tour-item" >
                                     <div class="tour-desc bg-white">
-                                        <div class="tour-text color-grey-3 text-center">&ldquo;At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.&rdquo;</div>
+                                        <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus mi, molestie ac risus at, rhoncus maximus nibh. Vestibulum iaculis purus quam, nec fermentum ex laoreet ut.&rdquo;</div>
                                         <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/6625914/pexels-photo-6625914.jpeg" alt=""></div>
                                         <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                         <div class="link-position d-flex justify-content-center">Student</div>
@@ -230,7 +230,7 @@
                             <div class="col-md-4 style-4">
                                 <div class="tour-item ">
                                     <div class="tour-desc bg-white">
-                                        <div class="tour-text color-grey-3 text-center">&ldquo;At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.&rdquo;</div>
+                                        <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus mi, molestie ac risus at, rhoncus maximus nibh. Vestibulum iaculis purus quam, nec fermentum ex laoreet ut.&rdquo;</div>
                                         <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
                                         <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                         <div class="link-position d-flex justify-content-center">Student</div>
@@ -240,7 +240,7 @@
                             <div class="col-md-4 style-4">
                                 <div class="tour-item ">
                                     <div class="tour-desc bg-white">
-                                        <div class="tour-text color-grey-3 text-center">&ldquo;At this School, our mission is to balance a rigorous comprehensive college preparatory curriculum with healthy social and emotional development.&rdquo;</div>
+                                        <div class="tour-text color-grey-3 text-center">&ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus mi, molestie ac risus at, rhoncus maximus nibh. Vestibulum iaculis purus quam, nec fermentum ex laoreet ut.&rdquo;</div>
                                         <div class="d-flex justify-content-center pt-2 pb-2"><img class="tm-people" src="https://images.pexels.com/photos/4946604/pexels-photo-4946604.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></div>
                                         <div class="link-name d-flex justify-content-center">Balbir Kaur</div>
                                         <div class="link-position d-flex justify-content-center">Student</div>
