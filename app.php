ac47540d80a42a0a9ba14d629618bdce6536081b
slyiin <batalunaleslyj@gmail.com>
2023-12-16 15:34:47 +0800

    add icons

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ;
+
+use Illuminate\Database\Eloquent\Model;
+
+class  extends Model
+{
+    protected $table = 'gallery';
+}
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,11 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="./img/favicon-16x16.ico" type="image/x-icon">
     <title>@yield('title', 'Default Title')</title>
     <script src="https://kit.fontawesome.com/2ecaabd2c2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/style.css">
+    
     {{-- @vite(['public/js/app.js']) --}}
 </head>
 
